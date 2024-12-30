1bf8f433f2358890f589ddae41d6d65ce560f82b
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-12-30 14:04:21 -0300

    feat: add bcrypt

--- src/core/services/auth.service.ts
+++ src/core/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { JwtService } from "@nestjs/jwt";
 import { SignInDto } from "../dtos/auth/sign-in.dto";
+import * as bcrypt from "bcrypt";
+import { CreateUserDto } from "../dtos/user/create-user.dto";
 
 @Injectable()
 export class AuthService {
@@ -9,12 +11,15 @@ export class AuthService {
     private userService: UserService,
     private jwtService: JwtService
   ) {}
+  saltOrRounds = 10;
 
   async signIn(SigninDto: SignInDto): Promise<{ access_token: string }> {
     const { email, password } = SigninDto;
-    const user = await this.userService.findOneByEmail(email);
 
-    if (user.password !== password) {
+    const user = await this.userService.findOneByEmail(email);
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
       throw new UnauthorizedException();
     }
 
@@ -28,5 +33,16 @@ export class AuthService {
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async signUp(payload: CreateUserDto) {
+    const hashPass = await bcrypt.hash(payload.password, this.saltOrRounds);
+
+    let data = {
+      ...payload,
+      password: hashPass,
+    };
+
+    return data;
   }
 }
